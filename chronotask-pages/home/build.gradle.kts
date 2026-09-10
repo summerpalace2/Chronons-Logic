07c86a1677de039103b525efe5600fb3438fcbca
summerpalace2 <2992203079@qq.com>
2026-09-10 12:08:03 +0800

    refactor(navigation): move tab composition to app layer

--- chronotask-pages/home/build.gradle.kts
+++ chronotask-pages/home/build.gradle.kts
@@ -11,12 +11,6 @@ useDB()
 dependencies {
     implementation(projects.chronotaskPages.home.api)
     implementation(projects.chronotaskPages.create.api)
-    implementation(projects.chronotaskPages.stats.api)
-    implementation(projects.chronotaskPages.stats)
-    implementation(projects.chronotaskPages.settings.api)
-    implementation(projects.chronotaskPages.settings)
     implementation(projects.chronotaskPages.taskdetail.api)
-    implementation(projects.chronotaskPages.notes)
-    implementation(projects.chronotaskPages.notes.api)
     implementation(projects.chronotaskComponents.ui)
 }
